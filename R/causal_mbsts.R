a447a5f5a3cbd9526f9e48e0c596443ec7b36181
FMenchetti <67006925+FMenchetti@users.noreply.github.com>
2020-06-22 17:03:39 +0200

    format R code

--- R/causal_mbsts.R
+++ R/causal_mbsts.R
@@ -15,105 +15,107 @@
 ######################################################################################
 
 
-causal.mbsts<-function(Smodel, X, y, dates, int.date, horizon = NULL, H = NULL, nu0.k = NULL, s0.k, 
-                       nu0.eps = NULL, s0.eps, niter, burn = NULL, ping = NULL){
-  
-  # It estimates a specific joint effect in a multivariate time series
-  # setting. It uses MCMC to sample from the joint posterior distribution of the parameters
-  # of an MBSTS model before the intervention/treatment occurred. Then, it uses the
-  # covariates post intervention to predict the counterfactual potential outcome 
-  # The prediction is done by sampling from the posterior predictive 
-  # distribution (ppd). Then the causal effect is computed by taking the difference between 
-  # the observed outcome of each time series and the mean of the ppd (credible intervals are
-  # computed accordingly).
-  
-  # Args:
-  #   Smodel   : a multivariate state space model of class 'SSModel'
-  #   X        : a T x N matrix of predictors
-  #   dates    : a vector of dates
-  #   int.date : date of the intervention
-  #   H        : desidered N x N variance-covariance matrix between regression coefficients.
-  #             The default is Zellner's g-prior, H = (X'X)^(-1) 
-  #   nu0.k    : degrees of freedom of the Inverse-Wishart prior for each Sigma_k.
-  #             The default is the smallest integer such that the expectation of eta_k exists,
-  #             that is, nu0.k = p + 2 where p is the number of time series in the 
-  #             multivariate model
-  #   nu0.eps  : degrees of freedom of the Inverse-Wishart prior for Sigma_eps, the default is p+2.
-  #   s0.k     : Scale matrix of the Inverse-Wishart prior for each Sigma_k.  
-  #   s0.eps   : Scale matrix of the Inverse-Wishart prior for Sigma.eps      
-  #   niter    : number of MCMC iteration
-  #   burn     : desidered burn-in, set by default to 0.1 * niter
-  #   ping     : logical, if TRUE a status message it's printed every iterations decile. 
-  #             Defaults to TRUE. 
-  #
-  # Value:
-  #   joint.effect : the estimated average causal effect and a 95% credible interval
-  #   joint        : joint causal effect for all iterations
-  #   mean.joint   : pointwise effect
-  #   lower.joint  : lower bound of the pointwise effect
-  #   upper.joint  : upper bound of the pointwise effect
-  #   mbsts        : an object of class 'mbsts'
-  #   predict      : a list with the same components as those produced by the function 'predict.mbsts'
-  #   adj.series   : observations in the analysis period excluding holidays
-  #   adj.dates    : dates in the analysis period without holidays
-  
-  ### STEP 1. Dividing pre and post periods
-  
-  ind<-dates < int.date
-  X.pre  <-X[ind,]
-  X.post <-X[!ind,]
-  y.pre  <-y[ind,]
-  y.post <-y[!ind,]
-  
-  # Estimating the model only in the pre-period
-  Smodel$y<-y.pre  
-  attr(Smodel,"n")<-as.integer(nrow(y.pre))
-  
-  ### STEP 2. MCMC 
-  
-  mbsts<-mbsts.mcmc(Smodel = Smodel, X = X.pre, H = NULL, nu0.k = nu0.k, s0.k = s0.k, nu0.eps = nu0.eps, 
-                    s0.eps = s0.eps, niter=niter, burn=burn, ping=ping)
-  
-  ### STEP 3. In- and out-of-sample forecasts from the ppd
-  predict<-predict.mbsts(mbsts, X.post)
- 
- 
-  ### STEP 4. Causal effect estimation 
-  p<-dim(mbsts$y)[2]
-  burn<-mbsts$burn
-  y.post.rep<-array(y.post, c(nrow(y.post),ncol(y.post),niter-burn)) 
-  y.diff<-y.post.rep-predict$post.pred.1 
-  
-  # removing holidays 
-  holi<-hol.dummy(dates[dates>=int.date]) 
-  y.diff<-y.diff[holi==0,,] 
-  adj.dates<-dates[hol.dummy(dates)==0]
-  
-  # Joint causal effect
-  
-  if(length(horizon)>0){ mean.effect<-list() ; lower.bound<-list() ; upper.bound<-list() ; joint.effect<-list()
-    for(i in 1:length(horizon)){
-      ind<-adj.dates[adj.dates>=int.date] <= horizon[i]
-      mean.effect[[i]]<-apply(y.diff[ind,,],c(1,2),mean) 
-      lower.bound[[i]]<-apply(y.diff[ind,,],c(1,2), quantile, probs = 0.025)
-      upper.bound[[i]]<-apply(y.diff[ind,,],c(1,2), quantile, probs = 0.975)
-      joint.effect[[i]]<-cbind(mean  = apply(colMeans(y.diff[ind,,]),1,mean), 
-                          lower = apply(colMeans(y.diff[ind,,]),1,quantile, probs=0.025),
-                          upper = apply(colMeans(y.diff[ind,,]),1,quantile, probs=0.975))
+causal.mbsts <- function(Smodel, X, y, dates, int.date, horizon = NULL, H = NULL, nu0.k = NULL, s0.k, 
+    nu0.eps = NULL, s0.eps, niter, burn = NULL, ping = NULL) {
+    
+    # It estimates a specific joint effect in a multivariate time series
+    # setting. It uses MCMC to sample from the joint posterior distribution of the parameters
+    # of an MBSTS model before the intervention/treatment occurred. Then, it uses the
+    # covariates post intervention to predict the counterfactual potential outcome 
+    # The prediction is done by sampling from the posterior predictive 
+    # distribution (ppd). Then the causal effect is computed by taking the difference between 
+    # the observed outcome of each time series and the mean of the ppd (credible intervals are
+    # computed accordingly).
+    
+    # Args:
+    #   Smodel   : a multivariate state space model of class 'SSModel'
+    #   X        : a T x N matrix of predictors
+    #   dates    : a vector of dates
+    #   int.date : date of the intervention
+    #   H        : desidered N x N variance-covariance matrix between regression coefficients.
+    #             The default is Zellner's g-prior, H = (X'X)^(-1) 
+    #   nu0.k    : degrees of freedom of the Inverse-Wishart prior for each Sigma_k.
+    #             The default is the smallest integer such that the expectation of eta_k exists,
+    #             that is, nu0.k = p + 2 where p is the number of time series in the 
+    #             multivariate model
+    #   nu0.eps  : degrees of freedom of the Inverse-Wishart prior for Sigma_eps, the default is p+2.
+    #   s0.k     : Scale matrix of the Inverse-Wishart prior for each Sigma_k.  
+    #   s0.eps   : Scale matrix of the Inverse-Wishart prior for Sigma.eps      
+    #   niter    : number of MCMC iteration
+    #   burn     : desidered burn-in, set by default to 0.1 * niter
+    #   ping     : logical, if TRUE a status message it's printed every iterations decile. 
+    #             Defaults to TRUE. 
+    #
+    # Value:
+    #   joint.effect : the estimated average causal effect and a 95% credible interval
+    #   joint        : joint causal effect for all iterations
+    #   mean.joint   : pointwise effect
+    #   lower.joint  : lower bound of the pointwise effect
+    #   upper.joint  : upper bound of the pointwise effect
+    #   mbsts        : an object of class 'mbsts'
+    #   predict      : a list with the same components as those produced by the function 'predict.mbsts'
+    #   adj.series   : observations in the analysis period excluding holidays
+    #   adj.dates    : dates in the analysis period without holidays
+    
+    ### STEP 1. Dividing pre and post periods
+    
+    ind <- dates < int.date
+    X.pre <- X[ind, ]
+    X.post <- X[!ind, ]
+    y.pre <- y[ind, ]
+    y.post <- y[!ind, ]
+    
+    # Estimating the model only in the pre-period
+    Smodel$y <- y.pre
+    attr(Smodel, "n") <- as.integer(nrow(y.pre))
+    
+    ### STEP 2. MCMC 
+    
+    mbsts <- mbsts.mcmc(Smodel = Smodel, X = X.pre, H = NULL, nu0.k = nu0.k, s0.k = s0.k, nu0.eps = nu0.eps, 
+        s0.eps = s0.eps, niter = niter, burn = burn, ping = ping)
+    
+    ### STEP 3. In- and out-of-sample forecasts from the ppd
+    predict <- predict.mbsts(mbsts, X.post)
+    
+    
+    ### STEP 4. Causal effect estimation 
+    p <- dim(mbsts$y)[2]
+    burn <- mbsts$burn
+    y.post.rep <- array(y.post, c(nrow(y.post), ncol(y.post), niter - burn))
+    y.diff <- y.post.rep - predict$post.pred.1
+    
+    # removing holidays 
+    holi <- hol.dummy(dates[dates >= int.date])
+    y.diff <- y.diff[holi == 0, , ]
+    adj.dates <- dates[hol.dummy(dates) == 0]
+    
+    # Joint causal effect
+    
+    if (length(horizon) > 0) {
+        mean.effect <- list()
+        lower.bound <- list()
+        upper.bound <- list()
+        joint.effect <- list()
+        for (i in 1:length(horizon)) {
+            ind <- adj.dates[adj.dates >= int.date] <= horizon[i]
+            mean.effect[[i]] <- apply(y.diff[ind, , ], c(1, 2), mean)
+            lower.bound[[i]] <- apply(y.diff[ind, , ], c(1, 2), quantile, probs = 0.025)
+            upper.bound[[i]] <- apply(y.diff[ind, , ], c(1, 2), quantile, probs = 0.975)
+            joint.effect[[i]] <- cbind(mean = apply(colMeans(y.diff[ind, , ]), 1, mean), lower = apply(colMeans(y.diff[ind, 
+                , ]), 1, quantile, probs = 0.025), upper = apply(colMeans(y.diff[ind, , ]), 1, quantile, 
+                probs = 0.975))
+        }
+    } else {
+        mean.effect <- apply(y.diff, c(1, 2), mean)
+        lower.bound <- apply(y.diff, c(1, 2), quantile, probs = 0.025)
+        upper.bound <- apply(y.diff, c(1, 2), quantile, probs = 0.975)
+        joint.effect <- cbind(mean = apply(colMeans(y.diff), 1, mean), lower = apply(colMeans(y.diff), 
+            1, quantile, probs = 0.025), upper = apply(colMeans(y.diff), 1, quantile, probs = 0.975))
     }
-  } else {
-    mean.effect<-apply(y.diff,c(1,2),mean) 
-    lower.bound<-apply(y.diff,c(1,2), quantile, probs = 0.025)
-    upper.bound<-apply(y.diff,c(1,2), quantile, probs = 0.975)
-    joint.effect<-cbind(mean  = apply(colMeans(y.diff),1,mean), 
-                        lower = apply(colMeans(y.diff),1,quantile, probs=0.025),
-                        upper = apply(colMeans(y.diff),1,quantile, probs=0.975))
-  }
-  
-  return(list(mcmc = mbsts, predict = predict, adj.series = y[hol.dummy(dates)==0,], 
-              adj.dates = adj.dates, joint = y.diff, joint.effect = joint.effect,  
-              mean.joint = mean.effect, lower.joint = lower.bound, upper.joint = upper.bound,
-              original.series = y, original.dates = dates))
+    
+    return(list(mcmc = mbsts, predict = predict, adj.series = y[hol.dummy(dates) == 0, ], adj.dates = adj.dates, 
+        joint = y.diff, joint.effect = joint.effect, mean.joint = mean.effect, lower.joint = lower.bound, 
+        upper.joint = upper.bound, original.series = y, original.dates = dates))
 }
 
 #--------------------------------------------------------------------------------------
--- R/predict_mbsts.R
+++ R/predict_mbsts.R
@@ -15,103 +15,110 @@
 
 
 
-predict.mbsts<-function(mbsts, newdata){
-  
-  # Given an object of class 'mbsts' and 'newdata' provided in matrix form, this function
-  # provides in-sample forecasts and out-of-sample forecasts, both based on drawing
-  # from the posterior predictive distribution. Note that NA values are not allowed in 
-  # the new regressor matrix. 
-  #
-  # Args:
-  #   mbsts   : an object of class 'mbsts'
-  #   newdata : matrix of new data
-  #
-  # Value:
-  #   post.pred.0 : T x p x niter array of in-sample forecasts
-  #   post.pred.1 : S x p x niter array out-of-sample forecasts, where S is the number of
-  #                 of forecasted periods (set to the length of provided new data)
-  #   post.pred   : (T + S) x p x niter array combining in- and out-of-sample forecasts
-  
-  ### Dimensionalities & other objects
-  
-  # get dim
-  t    <-dim(mbsts$y)[1]                  # number of time points
-  K    <-dim(mbsts$R)[2]                  # tot number of state disurbances
-  p    <-dim(mbsts$y)[2]                  # number of time series
-  k    <-K/p                              # number of state disturbances for each time series
-  M    <-dim(mbsts$T)[1]                  # tot number of states 
-  step <-dim(newdata)[1]                  # number of obs to forecast
-  niter<-mbsts$niter - mbsts$burn         # number of simulations
-  last <- dim(mbsts$eta.samples)[1]       # last draw from the posterior p(alpha_t|Y_n)
-  
-  
-  ### Empty arrays to store iterations
-  eta.new<-array(NA, c(step,K,niter))
-  
-  states.new<-array(NA, c(step,M,niter))
-  colnames(states.new)<-colnames(mbsts$states.samples)
-  
-  y.star<-array(NA, c(step,p,niter))
-  
-  post.pred.0<-array(NA, c(t,p,niter))           # storing in-sample draws  
-  post.pred.1<-array(NA, c(step,p,niter))        # storing out-of-samples draws 
-  post.pred  <-array(NA, c(t+step,p,niter))      # all together
-  
-  
-  for(i in 1:niter){
+predict.mbsts <- function(mbsts, newdata) {
     
-    ### STEP 1: Get in-samples draws from the ppd.
+    # Given an object of class 'mbsts' and 'newdata' provided in matrix form, this function
+    # provides in-sample forecasts and out-of-sample forecasts, both based on drawing
+    # from the posterior predictive distribution. Note that NA values are not allowed in 
+    # the new regressor matrix. 
+    #
+    # Args:
+    #   mbsts   : an object of class 'mbsts'
+    #   newdata : matrix of new data
+    #
+    # Value:
+    #   post.pred.0 : T x p x niter array of in-sample forecasts
+    #   post.pred.1 : S x p x niter array out-of-sample forecasts, where S is the number of
+    #                 of forecasted periods (set to the length of provided new data)
+    #   post.pred   : (T + S) x p x niter array combining in- and out-of-sample forecasts
     
-    # Details:  let theta = (alpha, beta, Sigma.eps, z, Sigma.eta),
-    #           during the MCMC we got samples from the joint posterior distribution p(theta|y). So
-    #           now we can draw new values y.tilde from the posterior predictive distribution by simply
-    #           taking the "niter" draws from the joint p(theta|y) and substitute them into model equations
-    #           (we assume that y.new comes from the same distribution of y so that y.new is 
-    #           independent of y given theta).
+    ### Dimensionalities & other objects
     
-    post.pred.0[,,i]<-tcrossprod(mbsts$states.samples[,,i], mbsts$Z[,,1]) + mbsts$X %*% mbsts$beta[,,i]  + mbsts$eps.samples[,,i]
+    # get dim
+    t <- dim(mbsts$y)[1]  # number of time points
+    K <- dim(mbsts$R)[2]  # tot number of state disurbances
+    p <- dim(mbsts$y)[2]  # number of time series
+    k <- K/p  # number of state disturbances for each time series
+    M <- dim(mbsts$T)[1]  # tot number of states 
+    step <- dim(newdata)[1]  # number of obs to forecast
+    niter <- mbsts$niter - mbsts$burn  # number of simulations
+    last <- dim(mbsts$eta.samples)[1]  # last draw from the posterior p(alpha_t|Y_n)
     
-    ### STEP 2: Get new out-of-samples draws from the ppd.
     
-    # Details: this time we should point out that an out-of sample draw y.new is no more independent
-    #          of past y given theta. To see that, let's say we want to sample y_t+k | Y_t.
-    #          This time, theta = (alpha_t+k,...,alpha_t+1, alpha_t, beta, Sigma.eps, z, Sigma.eta)
-    #          and from our MCMC we have just the posterior of (alpha_t, beta, Sigma.eps, z, Sigma.eta)
-    #          because we sampled from the full conditional p(alpha_t | Y_t, beta, Sigma.eps, z, Sigma.eta).
-    #          Thus, to sample from y_t+k | y_t we should "recover" the missing dependence structure
-    #          p(alpha_t+k,...,alpha_t+1 | theta') whre theta' is our "old" theta, that is 
-    #          theta' = alpha_t, beta, Sigma.eps, z, Sigma.eta. More details in the pdf. 
-    #          Conversely, y*_t+k =  y_t+k - Z alpha_t+k is independent of y*_t given theta',  
-    #          because there's no more time component and we can just write y*. 
-    #          During the MCMC we sampled from the joint posterior p(beta,Sigma.eps,z|y*),
-    #          and as in step 1 we can use those draws to sample from the 
-    #          posterior predictive distribution y.new* | y* by simply taking the draws from 
-    #          p(beta,Sigma.eps,z|y*) and then draw from p(y.new*|beta,Sigma.eps,z).  
-    #           
+    ### Empty arrays to store iterations
+    eta.new <- array(NA, c(step, K, niter))
     
-    # 2.1. Sampling new states
-    #if(seed){set.seed(seed)}
-    eta.new<-matrix(mvrnorm(1,rep(0,K),mbsts$Sigma.eta[,,i]),nrow=K)
-    states.new[1,,i]<-mbsts$T[,,1] %*% matrix(mbsts$states.samples[last,,i],M) + mbsts$R[,,1] %*% eta.new
+    states.new <- array(NA, c(step, M, niter))
+    colnames(states.new) <- colnames(mbsts$states.samples)
     
-    ind<-if(step>1){2:step} else {NULL}
-    for( j in ind){
-      eta.new<-matrix(mvrnorm(1,rep(0,K),mbsts$Sigma.eta[,,i]),nrow=K)
-      states.new[j,,i]<-mbsts$T[,,1] %*% matrix(states.new[j-1,,i],nrow=M) + mbsts$R[,,1]%*%eta.new 
+    y.star <- array(NA, c(step, p, niter))
+    
+    post.pred.0 <- array(NA, c(t, p, niter))  # storing in-sample draws  
+    post.pred.1 <- array(NA, c(step, p, niter))  # storing out-of-samples draws 
+    post.pred <- array(NA, c(t + step, p, niter))  # all together
+    
+    
+    for (i in 1:niter) {
+        
+        ### STEP 1: Get in-samples draws from the ppd.
+        
+        # Details:  let theta = (alpha, beta, Sigma.eps, z, Sigma.eta),
+        #           during the MCMC we got samples from the joint posterior distribution p(theta|y). So
+        #           now we can draw new values y.tilde from the posterior predictive distribution by simply
+        #           taking the 'niter' draws from the joint p(theta|y) and substitute them into model equations
+        #           (we assume that y.new comes from the same distribution of y so that y.new is 
+        #           independent of y given theta).
+        
+        post.pred.0[, , i] <- tcrossprod(mbsts$states.samples[, , i], mbsts$Z[, , 1]) + mbsts$X %*% mbsts$beta[, 
+            , i] + mbsts$eps.samples[, , i]
+        
+        ### STEP 2: Get new out-of-samples draws from the ppd.
+        
+        # Details: this time we should point out that an out-of sample draw y.new is no more independent
+        #          of past y given theta. To see that, let's say we want to sample y_t+k | Y_t.
+        #          This time, theta = (alpha_t+k,...,alpha_t+1, alpha_t, beta, Sigma.eps, z, Sigma.eta)
+        #          and from our MCMC we have just the posterior of (alpha_t, beta, Sigma.eps, z, Sigma.eta)
+        #          because we sampled from the full conditional p(alpha_t | Y_t, beta, Sigma.eps, z, Sigma.eta).
+        #          Thus, to sample from y_t+k | y_t we should 'recover' the missing dependence structure
+        #          p(alpha_t+k,...,alpha_t+1 | theta') whre theta' is our 'old' theta, that is 
+        #          theta' = alpha_t, beta, Sigma.eps, z, Sigma.eta. More details in the pdf. 
+        #          Conversely, y*_t+k =  y_t+k - Z alpha_t+k is independent of y*_t given theta',  
+        #          because there's no more time component and we can just write y*. 
+        #          During the MCMC we sampled from the joint posterior p(beta,Sigma.eps,z|y*),
+        #          and as in step 1 we can use those draws to sample from the 
+        #          posterior predictive distribution y.new* | y* by simply taking the draws from 
+        #          p(beta,Sigma.eps,z|y*) and then draw from p(y.new*|beta,Sigma.eps,z).  
+        #           
+        
+        # 2.1. Sampling new states
+        #if(seed){set.seed(seed)}
+        eta.new <- matrix(mvrnorm(1, rep(0, K), mbsts$Sigma.eta[, , i]), nrow = K)
+        states.new[1, , i] <- mbsts$T[, , 1] %*% matrix(mbsts$states.samples[last, , i], M) + mbsts$R[, 
+            , 1] %*% eta.new
+        
+        ind <- if (step > 1) {
+            2:step
+        } else {
+            NULL
+        }
+        for (j in ind) {
+            eta.new <- matrix(mvrnorm(1, rep(0, K), mbsts$Sigma.eta[, , i]), nrow = K)
+            states.new[j, , i] <- mbsts$T[, , 1] %*% matrix(states.new[j - 1, , i], nrow = M) + mbsts$R[, 
+                , 1] %*% eta.new
+        }
+        
+        # 2.2. Sampling y*.new.
+        
+        y.star[, , i] <- newdata %*% mbsts$beta[, , i] + mvrnorm(1, rep(0, p), mbsts$Sigma.eps[, , i])
+        
+        # 2.3. Out-of-sample draws
+        
+        post.pred.1[, , i] <- states.new[, , i] %*% t(mbsts$Z[, , 1]) + y.star[, , i]
+        
+        ### STEP 3: Combining in-sample and out-of-samples draws
+        
+        post.pred[, , i] <- rbind(post.pred.0[, , i], post.pred.1[, , i])
     }
     
-    # 2.2. Sampling y*.new.
-    
-    y.star[,,i]<-newdata %*% mbsts$beta[,,i] + mvrnorm(1, rep(0,p), mbsts$Sigma.eps[,,i])
-    
-    # 2.3. Out-of-sample draws
-    
-    post.pred.1[,,i]<-states.new[,,i] %*% t(mbsts$Z[,,1]) + y.star[,,i]
-    
-    ### STEP 3: Combining in-sample and out-of-samples draws
-    
-    post.pred[,,i]<-rbind(post.pred.0[,,i], post.pred.1[,,i])
-  } 
-  
-  return(list(post.pred.0 = post.pred.0, post.pred.1 = post.pred.1, post.pred = post.pred ))  
+    return(list(post.pred.0 = post.pred.0, post.pred.1 = post.pred.1, post.pred = post.pred))
 }
