feba448e7f5a938f6ae152cd5c0b1e0618ab03dc
Victoria L. Prince <vliublinska@hbs.edu>
2020-08-18 18:55:09 -0400

    Added summary.CausalMBSTS and print.summary.CausalMBSTS methods

--- R/print_CausalMBSTS.R
+++ R/print_CausalMBSTS.R
@@ -1,15 +1,22 @@
 
-#' Printing method for an object of class CausalMBSTS
+#' Summarize results of causal effect estimation with CausalMBSTS
 #'
-#' The method prints the results of a causal analysis in a nice and concise output format.
+#' The method extracts and computes various results of the causal analysis with CausalMBSTS.
 #'
-#' @param x An object of class 'CausalMBSTS'.
+#' @param x An object of class 'CausalMBSTS', a result of a call to \code{\link{CausalMBSTS}}.
 #'
-#' @return
-#' @export
+#' @return Returns an object of class \code{summary.CausalMBSTS}, which is a list of data frames corresponding to each
+#' date provided in \code{horizon} (or its default value) with the following columns:
+#'   \item{mean}{Estimated average causal effect.}
+#'   \item{lower}{Lower bound of the two-sided (1-\code{alpha})\% credible interval.}
+#'   \item{upper}{Upper bound of the two-sided (1-\code{alpha})\% credible interval.}
+#'   \item{cum.sum}{Pointwise effect}
+#'   \item{lower.general}{Lower bound of a (1-\code{alpha})\% credible interval of the pointwise effect.}
+#'   \item{upper.general}{Upper bound of a (1-\code{alpha})\% credible interval of the pointwise effect.}
+#'   \item{bayes.pval}{Bayesian p-value for the average causal effect.}
+#'   \item{pct.causal.eff}{Probability of a causal effect (\%)}
 #'
 #' @examples
-#' ## Example 2
 #' set.seed(1)
 #' t <- seq(from = 0,to = 4*pi, length.out=300)
 #' y <- cbind(3*sin(2*t)+rnorm(300), 2*cos(2*t) + rnorm(300))
@@ -17,14 +24,17 @@
 #' int.date <- as.Date("2020-02-27")
 #' y[dates >= int.date,] <- y[dates >= int.date,]+2
 #'
-#' # Model definition
-#' model.2 <- model(y, components = c("trend", "cycle"), cycle.period = 75)
-#' causal.2 <- causal.mbsts(model.2, dates = dates, int.date = int.date, s0.r = 0.01*diag(2), s0.eps = 0.1*diag(2), niter = 100, burn = 10)
+#' # Causal effect estimation
+#' causal.2 <- CausalMBSTS(y, components = c("trend", "cycle"), cycle.period = 75,
+#'                         dates = dates, int.date = int.date, s0.r = 0.01*diag(2),
+#'                         s0.eps = 0.1*diag(2), niter = 100, burn = 10)
 #'
-#' # Print
-#' print(causal.2)
-
-print.CausalMBSTS <- function(x){
+#' sum.causal.2 <- summary(causal.2)
+#' print(sum.causal.2, digits = 2)
+#' sum.causal.2$horizon_default
+#'
+#' @export
+summary.CausalMBSTS <- function(x){
 
   effect <- x$general.effect
   pred <- x$predict$post.pred
@@ -33,61 +43,73 @@ print.CausalMBSTS <- function(x){
   summary_impact <- list()
 
   if (is.list(effect)){
-    effect <- lapply(effect, round, digits = 2)
-
     for(i in 1:length(effect)){
-
-      # effect
-      average <- effect[[i]][,"mean"]
-      cumulative <- effect[[i]][,"cum.sum"]
-      average.ci <- paste("[", effect[[i]][,"lower"], ", ", effect[[i]][,"upper"], "]" , sep = "")
-      cumulative.ci <- paste("[", effect[[i]][,"cum.lower"], ", ", effect[[i]][,"cum.upper"], "]" , sep = "")
-      impact <- data.frame( average, average.ci, cumulative, cumulative.ci)
-      colnames(impact) <- c("Avg. Effect", "95% CI", "Cumulative Effect", "95% CI")
 
       # Bayesian p-value
       end <- start + dim(x$mean.general[[i]])[1] -1 # interval between the intervention and the time horizon
+      print(end)
       true.sum <- colSums(x$y[start:end,])
       pred.sum <- apply(pred[start:end,,], c(2,3), sum) # cumulative causal effect at each iteration
       replicas <- cbind(pred.sum,true.sum) # cumulative sum of the observations in each replicated data set
-      p <- apply(rbind(rowSums(replicas >= true.sum)/(niter + 1),rowSums(replicas <= true.sum)/(niter+1)),2,min)
-      prob_causal_eff <- data.frame(round(p,4), (1-round(p,4))*100)
-      rownames(prob_causal_eff) <- rownames(impact) <- dimnames(x$y)[[2]]
-      colnames(prob_causal_eff) <- c("Bayesian p-value", "Prob. of a causal effect (%)")
+      p <- apply(rbind(rowSums(replicas >= true.sum)/(niter + 1), rowSums(replicas <= true.sum)/(niter+1)),2,min)
+      prob_causal_eff <- data.frame(bayes.pval = p, pct.causal.eff = (1-p)*100)
+
+      # Outputting estimated effect(s) and p-values(s)
+      output <- cbind(effect[[i]], prob_causal_eff)
+      rownames(output) <-dimnames(x$y)[[2]]
 
       # saving output
-      summary_impact[[i]] <- list(impact, prob_causal_eff)
-      names(summary_impact[[i]]) <- c("Effect", "P-value")
+      summary_impact[[i]] <- output
     }
 
-    names(summary_impact) <- paste("horizon",1:length(effect))
+    names(summary_impact) <- paste0("horizon",1:length(effect))
 
   } else {
-    effect <- round(effect, digits = 2)
-
-    # effect
-    average <- effect[,"mean"]
-    cumulative <- effect[,"cum.sum"]
-    average.ci <- paste("[", effect[,"lower"], ", ", effect[,"upper"], "]" , sep = "")
-    cumulative.ci <- paste("[", effect[,"cum.lower"], ", ", effect[,"cum.upper"], "]" , sep = "")
-    impact <- data.frame( average, average.ci, cumulative, cumulative.ci)
-    colnames(impact) <- c("Avg. Effect", "95% CI", "Cumulative Effect", "95% CI")
 
     # Bayesian p-value
-
     end <- dim(x$y)[1]
     true.sum <- colSums(x$y[start:end,])
     pred.sum <- apply(pred[start:end,,], c(2,3), sum) # cumulative causal effect at each iteration
     replicas <- cbind(pred.sum,true.sum) # cumulative sum of the observations in each replicated data set
-    p <- apply(rbind(rowSums(replicas >= true.sum)/(niter + 1),rowSums(replicas <= true.sum)/(niter+1)),2,min)
-    prob_causal_eff <- data.frame(round(p,4), (1-round(p,4))*100)
-    rownames(prob_causal_eff) <- rownames(impact) <- dimnames(x$y)[[2]]
-    colnames(prob_causal_eff) <- c("Bayesian p-value", "Prob. of a causal effect (%)")
+    p <- apply(rbind(rowSums(replicas >= true.sum)/(niter + 1), rowSums(replicas <= true.sum)/(niter+1)),2,min)
+    prob_causal_eff <- data.frame(bayes.pval = p, pct.causal.eff = (1-p)*100)
+
+    # Outputting estimated effect(s) and p-values(s)
+    output <- cbind(effect, prob_causal_eff)
+    rownames(output) <- dimnames(x$y)[[2]]
 
     # saving output
-    summary_impact<- list(impact, prob_causal_eff)
-    names(summary_impact) <- c("Effect", "P-value")
+    summary_impact[["horizon_default"]] <- output
   }
 
-  print(summary_impact)
+  class(summary_impact) <- "summary.CausalMBSTS"
+  summary_impact
+}
+
+
+
+#' Format and print the estimated causal effect(s), credible interval(s), and Bayesian p-value(s) into a clear output.
+#'
+#'
+#' @param object An object of class 'summary.CausalMBSTS', a result of a call to \code{\link{summary.CausalMBSTS}}.
+#' @param digits Number of digits to display.
+#' @param ... Additional arguments passed to other methods.
+#' @return Invisibly, \code{object}.
+#' @export
+
+print.summary.CausalMBSTS <- function(object, digits = max(3, getOption("digits") - 3), ...){
+
+  for(i in 1:length(object)){
+    object[[i]] <- round(object[[i]], digits = digits)
+    cat("\nResults for ",names(object)[i],":\n\n", sep="")
+
+    # effect
+    average.ci <- paste("(", object[[i]][,"lower"], ",", object[[i]][,"upper"], ")" , sep = "")
+    cumulative.ci <- paste("(", object[[i]][,"cum.lower"], ",", object[[i]][,"cum.upper"], ")" , sep = "")
+    impact <- data.frame(object[[i]][,"mean"], average.ci, object[[i]][,"cum.sum"], cumulative.ci, object[[i]][,"bayes.pval"], object[[i]][,"pct.causal.eff"])
+    colnames(impact) <- c("Avg. Effect", "95% CI", "Cumulative Effect", "95% CI", "Bayesian p-value", "Prob. of a causal effect (%)")
+    print(impact)
+  }
+  cat("\n")
+  return(invisible(object))
 }
