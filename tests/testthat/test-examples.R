7b302196a23b8534e9c8270d419082bad330ac36
Ista Zahn <izahn@hbs.edu>
2020-08-26 07:52:32 -0400

    don't check examples twice

--- tests/testthat/test-examples.R
+++
@@ -1,8 +0,0 @@
-test_that("Package examples work", {
-    tmp <- tempfile()
-    sink(tmp)
-    ex <- try(devtools::run_examples('../..'))
-    sink()
-    unlink(tmp)
-    expect_true(class(ex) != 'try-error')
-})
